30855b8bed642e2baa30cd45a5cc121f869a2419
wilshdog <31513793+wilshdog@users.noreply.github.com>
2017-11-07 08:30:46 -0700

    Added a test for the cute animal memes. Still not working must make it work.

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -9,37 +9,37 @@ public class Chatbot
 	private List<Movie> movieList;
 	private List<String> shoppingList;
 	private List<String> cuteAnimalMemes;
-	private String [] verbs;
-	private String [] topics;
-	private String [] followUps;
-	private String [] questions;
+	private String[] verbs;
+	private String[] topics;
+	private String[] followUps;
+	private String[] questions;
 	private String username;
 	private String content;
 	private String intro;
 	private LocalTime currentTime;
-	
+
 	public Chatbot(String username)
 	{
 		this.movieList = null;
 		this.shoppingList = new ArrayList<String>();
 		this.cuteAnimalMemes = null;
 		this.currentTime = null;
-		this.questions = new String [10];
+		this.questions = new String[10];
 		this.username = username;
 		this.content = null;
 		this.intro = null;
 		this.currentTime = null;
-		this.topics = new String [7];
-		this.verbs = new String [4];
-		this.followUps = new String [5];
-		
+		this.topics = new String[7];
+		this.verbs = new String[4];
+		this.followUps = new String[5];
+
 		buildVerbs();
 		buildTopics();
 		buildFollowups();
 		buildQuestions();
 		buildShoppingList();
 	}
-	
+
 	private void buildVerbs()
 	{
 		verbs[0] = "like";
@@ -47,7 +47,7 @@ public class Chatbot
 		verbs[2] = "am ambivalent about";
 		verbs[3] = "am thinking about";
 	}
-	
+
 	private void buildTopics()
 	{
 		topics[0] = "School";
@@ -58,27 +58,30 @@ public class Chatbot
 		topics[5] = "TV Shows";
 		topics[6] = "Sports";
 	}
-	
+
 	private void buildFollowups()
 	{
-	
+
 	}
 
 	private void buildMovieList()
 	{
-		
+
 	}
-	
+
 	private void buildShoppingList()
 	{
-		
+
 	}
-	
+
 	private void buildCuteAnimals()
 	{
+		cuteAnimalMemes.add("otter");
+		cuteAnimalMemes.add("pupper");
+		cuteAnimalMemes.add("kittie");
 		
 	}
-	
+
 	private void buildQuestions()
 	{
 		questions[0] = "How was your day?";
@@ -92,69 +95,89 @@ public class Chatbot
 		questions[8] = "Who is your favorite actor?";
 		questions[9] = "What color are your pants?";
 	}
-	
+
 	public String processConversation(String input)
 	{
 		String chatbotResponse = "";
 		chatbotResponse += "You said: " + "\n" + input + "\n";
 		chatbotResponse += buildChatbotResponse();
-		
+
 		return chatbotResponse;
 	}
-	
+
 	private String buildChatbotResponse()
 	{
 		String response = "I";
 		int random = (int) (Math.random() * verbs.length);
-		
+
 		response += verbs[random];
-		
+
 		random = (int) (Math.random() * topics.length);
 		response += " " + topics[random] + ".\n";
-		
+
 		random = (int) (Math.random() * questions.length);
 		response += questions[random];
-		
+
 		return response;
-		
-		
+
 	}
-	
+
 	public boolean lengthChecker(String input)
 	{
 		return false;
 	}
-	
+
 	public boolean htmlTagChecker(String input)
 	{
 		return false;
 	}
-	
+
 	public boolean userNameChecker(String input)
 	{
 		return false;
 	}
-	
+
 	public boolean contentChecker(String contentCheck)
 	{
 		return false;
 	}
-	
+
 	public boolean cuteAnimalMemeChecker(String input)
 	{
+		int index = 0;
+
+		for (index = 0; index < cuteAnimalMemes.size(); index += 1)
+		{
+			if (input.contains("otter") || input.contains("kittie") || input.contains("pupper"))
+			{
+				return true;
+			}
+
+		}
+
 		return false;
 	}
-	
+
 	public boolean shoppingListChecker(String shoppingItem)
 	{
+		int index = 0;
+
+		for (index = 0; index < shoppingList.size(); index += 1)
+		{
+			if (shoppingItem.contains("protien") || shoppingItem.contains("veggies") || shoppingItem.contains("snacks"))
+			{
+				return true;
+			}
+		}
+
 		return false;
 	}
-	
+
 	public boolean movieTitleChecker(String title)
 	{
 		return false;
 	}
-	
+
 	public boolean movieGenreChecker(String genre)
 	{
 		return false;
@@ -169,27 +192,27 @@ public class Chatbot
 	{
 		return false;
 	}
-	
+
 	public List<Movie> getMovieList()
 	{
 		return movieList;
 	}
-	
+
 	public List<String> getShoppingList()
 	{
 		return shoppingList;
 	}
-	
+
 	public List<String> getCuteAnimalMemes()
 	{
 		return cuteAnimalMemes;
 	}
 
-	public String [] getQuestions()
+	public String[] getQuestions()
 	{
 		return null;
 	}
-	
+
 	public String[] getVerbs()
 	{
 		return verbs;
@@ -209,7 +232,7 @@ public class Chatbot
 	{
 		return username;
 	}
-	
+
 	public String getContent()
 	{
 		return content;
@@ -219,17 +242,17 @@ public class Chatbot
 	{
 		return null;
 	}
-	
+
 	public LocalTime getCurrentTime()
 	{
 		return null;
 	}
-	
+
 	public void setUsername(String username)
 	{
 		this.username = username;
 	}
-	
+
 	public void setContent(String content)
 	{
 		this.content = content;
