2458a1bacbc8d2123823ccdea970a5dac25b63e9
wilshdog <31513793+wilshdog@users.noreply.github.com>
2017-11-15 08:48:34 -0700

    Added a chat frame.

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -5,5 +5,17 @@ import javax.swing.JFrame;
 
 public class ChatFrame extends JFrame
 {
-
+	private ChatbotController appController;
+	
+	public ChatFrame(ChatbotController appController)
+	{
+		super();
+		this.appController = appController;
+		
+	}
+	
+	private void setupFrame()
+	{
+		this.setVisible(true);
+	}
 }
