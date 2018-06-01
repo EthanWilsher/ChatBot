d94cb8ea605ba21811ac21ef202caaf8587fe686
wilshdog <31513793+wilshdog@users.noreply.github.com>
2018-06-01 07:34:55 -0600

    Final Commit

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -5,9 +5,11 @@ import twitter4j.*;
 
 import java.util.Scanner;
 import java.util.List;
+import java.util.Map;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.text.DecimalFormat;
+import java.util.Hashtable;
 
 public class CTECTwitter
 {
@@ -18,6 +20,7 @@ public class CTECTwitter
 	private long totalWordCount;
 	private HashMap<String, Integer> wordsAndCount;
 	
+	
 	public CTECTwitter(ChatbotController appController)
 	{
 		this.appController = appController;
@@ -25,6 +28,58 @@ public class CTECTwitter
 		this.searchedTweets = new ArrayList<Status>();
 		this.tweetedWords = new ArrayList<String>();
 		this.totalWordCount = 0;
+	}
+	
+	private ArrayList<Map.Entry<String, Integer>> sortHashMap()
+	{
+		
+		
+	}
+	
+	public String analyzeTwitterForTopic(String topic)
+	{
+		String results = "";
+		searchedTweets.clear();
+		Query twitterQuery = new Query(topic);
+		int resultMax = 750;
+		long lastId = Long.MAX_VALUE;
+		double latitude = 40.7128;
+		double longitude = 30;
+		double radius = 30.5;
+		
+		twitterQuery.setGeoCode(new GeoLocation location, double radius, Query.Unit unit);
+		ArrayList<Status> matchingTweets = new ArrayList<Status>();
+		while(searchedTweets.size() < resultMax)
+		{
+			try
+			{
+				QueryResult resultingTweets = chatbotTwitter.search(twitterQuery);
+				
+				for(Status currentTweet : resultingTweets.getTweets())
+				{
+					
+					if (currentTweet.getId() < lastId)
+					{
+						matchingTweets.add(currentTweet);
+						lastId = currentTweet.getId();
+					}
+				}
+			}
+			catch(TwitterException error)
+			{
+				appController.handleErrors(error);
+			}
+			
+			twitterQuery.setMaxId(lastId - 1);
+		}
+		
+		results += "Talk about the search results";
+		results += "Find a tweet that will pass one of the checkers in chatbot";
+		
+		int randomTweet = (int) (Math.random() * matchingTweets.size());
+		results += matchingTweets.get(randomTweet);
+		
+		return results;
 	}
 	
 	public void sendTweet(String textToTweet)
@@ -44,6 +99,8 @@ public class CTECTwitter
 		
 	}
 	
+	
+	
 	public String getMostCommonWord(String username)
 	{
 		String mostCommon = "";
@@ -55,6 +112,18 @@ public class CTECTwitter
 		trimTheBoringWords(boring);
 		removeBlanks();
 		generateWordCount();
+		
+		ArrayList<Map.Entry<String, Integer>> sorted = sortHashMap();
+		
+		int maxWord = 0;
+		String mostCommonWord = sorted.get(0).getKey();
+		
+		maxWord = sorted.get(0).getValue();
+		
+		mostCommon = "The common word in " + username + "'s " + searchedTweets.size() + " tweets is " + mostCommonWord + ", and it was used " + maxWord + "times. \nThis is " + (DecimalFormat.getPercentInstance().format(((double) maxWord)/totalWordCount))
+				+ " of total words: " + totalWordCount + " and is " + (DecimalFormat.getPercentInstance().format(((double) maxWord)/wordsAndCount.size())) + " of the unique words: " + wordsAndCount.size();
+				
+		
 		
 		return mostCommon;
 	}
@@ -72,8 +141,27 @@ public class CTECTwitter
 			   
 	}
 	
+	private ArrayList<Map.Entry<String, Integer>> sort HashMap()
+	{
+		ArrayList<Map.Entry<String, Integer>> entries = new ArrayList<Map.Entry<String, Integer>>(wordsAndCount.entrySet());
+		entries.sort((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue())); 
+		
+		return entries;
+	}
+	
 	private void generateWordCount()
 	{
+		for (String word : tweetedWords)
+		{
+			if(!wordsAndCount.containsKey(word.toLowerCase()))
+			{
+				wordsAndCount.put(word.toLowerCase(), 1);
+			}
+			else
+			{
+				wordsAndCount.replace(word.toLowerCase(), wordsAndCount.get(word.toLowerCase()) + 1);
+			}
+		}
 		
 	}
 	
@@ -180,6 +268,44 @@ public class CTECTwitter
 		
 		wordScanner.close();
 		return boringWords;
+	}
+	
+	
+	private String sortedWords()
+	{
+		String allWords = "";
+		String [] words = new String [wordsAndCount.size()];
+		ArrayList<String> wordList = new ArrayList<String>(wordsAndCount.keySet());
+		for(int index = 0; index < wordsAndCount.size(); index++)
+		{
+			words[index] = wordList.get(index);
+		}
+		
+		for(int index = 0; index < words.length - 1; index++)
+		{
+			int maxIndex = index;
+		
+			for (int inner = index + 1; inner < words.length; inner++)
+			{
+				if(words[inner].compareTo(words[maxIndex]) > 0)
+				{
+					maxIndex = inner;
+				}
+				
+			}
+			
+			String tempMax = words[maxIndex];
+			words[maxIndex] = words[index];
+			words[index] = tempMax;
+
+		}
+		
+		for (String word : words)
+		{
+			allWords += word + ", ";
+		}
+		
+		return allWords;
 	}
 
 }
